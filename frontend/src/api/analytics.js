04a4b82785012b1194e046fb3a888a6d3d7cb3b2
Nasheer <nasheer24555a4406@gmail.com>
2026-08-21 18:59:26 +0530

    Remove project files from main branch

--- frontend/src/api/analytics.js
+++
@@ -1,6 +0,0 @@
-import api from "./axios";
-
-export const getUserAnalytics = (userId = null) => {
-  const url = userId ? `/analytics?user_id=${userId}` : "/analytics";
-  return api.get(url).then(res => res.data);
-};
